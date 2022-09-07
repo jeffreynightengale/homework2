05432cf5a73944887362466f62e852f7b638cd28
Jeffrey Nightengale <78056207+jeffreynightengale@users.noreply.github.com>
2022-09-07 14:20:06 -0500

    Update header.php

--- header.php
+++ header.php
@@ -4,7 +4,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
-  </head>
 <nav class="navbar navbar-expand-lg bg-light">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Navbar</a>
