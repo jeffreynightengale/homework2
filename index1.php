4cc08a10c028b722c7f6a512bbe89b62ecf453f6
Jeffrey Nightengale <78056207+jeffreynightengale@users.noreply.github.com>
2022-09-08 13:38:51 -0500

    Create index1.php

---
+++ index1.php
@@ -0,0 +1,10 @@
+<?php require_once("header.php"); ?>
+<h1> Hello World </h1>
+<form action="handlepost1.php" method="post">
+Favorite color: <input type="text" name="color"><br>
+Favorite animal: <input type="text" name="animal"><br>
+<input type="submit">
+</form>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+  </body>
+</html>
