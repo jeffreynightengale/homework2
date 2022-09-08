b387ede696ea5c007007a2e5559e6c60cf70d5d5
Jeffrey Nightengale <78056207+jeffreynightengale@users.noreply.github.com>
2022-09-08 13:44:30 -0500

    Update handlepost1.php

--- handlepost1.php
+++ handlepost1.php
@@ -7,8 +7,8 @@
     <?php require_once("header.php"); ?>
   </head>
   <body>
-Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
+Welcome <?php echo $_GET["name"]; ?><br>
+Your email address is: <?php echo $_GET["email"]; ?>
     You really like the color, <? echo $_GET["color"]; ?>
     Your favorite animal is <? echo $_GET["animal"]; ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
