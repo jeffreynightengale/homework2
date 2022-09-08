cec24fe843ec8e8dff863e0749b304fb9e66bd58
Jeffrey Nightengale <78056207+jeffreynightengale@users.noreply.github.com>
2022-09-08 13:43:46 -0500

    Update index1.php

--- index1.php
+++ index1.php
@@ -1,6 +1,8 @@
 <?php require_once("header.php"); ?>
 <h1> Hello World </h1>
 <form action="handlepost1.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
 Favorite color: <input type="text" name="color"><br>
 Favorite animal: <input type="text" name="animal"><br>
 <input type="submit">
