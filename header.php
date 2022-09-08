d21390c01b6be9739eb33984c683e7ab9a2a930a
Jeffrey Nightengale <78056207+jeffreynightengale@users.noreply.github.com>
2022-09-08 13:37:12 -0500

    Update header.php

--- header.php
+++ header.php
@@ -8,17 +8,17 @@
   </head>
 <nav class="navbar navbar-expand-lg bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="index.php">Home</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="">Using Post</a>
+          <a class="nav-link active" aria-current="page" href="index.php">Using Post</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="">Using Get</a>
+          <a class="nav-link" href="index.php">Using Get</a>
         </li>
       </ul>
     </div>
