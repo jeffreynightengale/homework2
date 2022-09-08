ade3a1b72d014703214bc2430a5664dc005c04ac
Jeffrey Nightengale <78056207+jeffreynightengale@users.noreply.github.com>
2022-09-08 13:34:33 -0500

    Update header.php

--- header.php
+++ header.php
@@ -15,16 +15,10 @@
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link active" aria-current="page" href="">Using Post</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled">Disabled</a>
+          <a class="nav-link" href="">Using Get</a>
         </li>
       </ul>
     </div>
