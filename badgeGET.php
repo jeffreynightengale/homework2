0176e77dd4a4f00a5b3faee7fd6132146233a479
Jeffrey Nightengale <78056207+jeffreynightengale@users.noreply.github.com>
2022-09-12 12:55:12 -0500

    Create badgeGET.php

---
+++ badgeGET.php
@@ -0,0 +1,4 @@
+<!doctype html>
+<html lang="en">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  <h1>Congrats! You used the GET function for PHP <span class="badge bg-secondary">New</span></h1>
