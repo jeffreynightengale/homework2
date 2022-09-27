546ea29fbe210326fe963749b6a21a06cd954a16
Jeffrey Nightengale <78056207+jeffreynightengale@users.noreply.github.com>
2022-09-27 13:43:04 -0500

    Update handlepost.php

--- handlepost.php
+++ handlepost.php
@@ -10,12 +10,6 @@
   <body>
 Welcome <?php echo $_POST["name"]; ?><br>
 Your email address is: <?php echo $_POST["email"]; ?>
-    <?php
-    function H(){
-      echo "You are doing great!";
-    }
-    
-    H();
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
   </body>
 </html>
